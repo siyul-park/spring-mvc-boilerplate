17e0f7fcaf1b31bea9196a1ff6b21257cdcb801b
Ara Park <kdPark0723@gmail.com>
2021-03-28 22:34:48 +0900

    feat: set up spring (#1)
    * chang gradle
    
    * Add files
    
    * move pacakge
    
    * remove application
    
    * Apply lint

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,19 @@
-rootProject.name = "kotlin-boilerplate"
+rootProject.name = "spring-mvc-boilerplate"
 
 pluginManagement {
+    val kotlin_version: String by settings
     val klint_version: String by settings
+    val spring_boot_version: String by settings
+    val spring_dependency_management_version: String by settings
 
     plugins {
+        id("org.springframework.boot") version spring_boot_version
+        id("io.spring.dependency-management") version spring_dependency_management_version
+
+        kotlin("jvm") version kotlin_version
+        kotlin("plugin.spring") version kotlin_version
+        kotlin("plugin.jpa") version kotlin_version
+
         id("org.jlleitschuh.gradle.ktlint") version klint_version
     }
 }
