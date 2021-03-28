bf9668b21371c0643c086d5553e48eae4f145c1f
siyual-park <kdpark0723@gmail.com>
2021-03-29 01:00:38 +0900

    fix test

--- src/main/kotlin/io/github/siyual_park/controller/ArticleController.kt
+++ src/main/kotlin/io/github/siyual_park/controller/ArticleController.kt
@@ -6,10 +6,12 @@ import io.github.siyual_park.repository.ArticleRepository
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/articles")
+@RestController
+@RequestMapping("/articles")
 class ArticleController(
     private val articleRepository: ArticleRepository
 ) {
