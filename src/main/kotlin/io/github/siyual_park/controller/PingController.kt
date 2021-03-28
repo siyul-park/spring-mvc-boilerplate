5489ef5f794744f96772678378c72e3cc2999d51
siyual-park <kdpark0723@gmail.com>
2021-03-28 23:22:00 +0900

    refactoring

--- src/main/kotlin/io/github/siyual_park/controller/PingController.kt
+++ src/main/kotlin/io/github/siyual_park/controller/PingController.kt
@@ -1,14 +1,14 @@
 package io.github.siyual_park.controller
 
-import org.springframework.http.ResponseEntity
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class PingController {
 
     @GetMapping("/ping")
-    fun ping(): ResponseEntity<String> {
-        return ResponseEntity.ok("pong")
-    }
+    @ResponseStatus(HttpStatus.OK)
+    fun ping() = "pong"
 }
--- src/main/kotlin/io/github/siyual_park/controller/VersionController.kt
+++ src/main/kotlin/io/github/siyual_park/controller/VersionController.kt
@@ -2,20 +2,19 @@ package io.github.siyual_park.controller
 
 import com.jcabi.manifests.Manifests
 import io.github.siyual_park.model.Version
-import org.springframework.http.ResponseEntity
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class VersionController {
 
     @GetMapping("/version")
-    fun ping(): ResponseEntity<Version> {
-        val version = Version(
-            javaClass.`package`.specificationVersion,
-            javaClass.`package`.implementationVersion,
-            Manifests.DEFAULT["Build-Time"]
-        )
-        return ResponseEntity.ok(version)
-    }
+    @ResponseStatus(HttpStatus.OK)
+    fun ping() = Version(
+        javaClass.`package`.specificationVersion,
+        javaClass.`package`.implementationVersion,
+        Manifests.DEFAULT["Build-Time"]
+    )
 }
