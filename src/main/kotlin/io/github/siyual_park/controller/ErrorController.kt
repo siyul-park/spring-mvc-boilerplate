8cf92f16a654a759b39a694d085f10d6758da36e
siyual-park <kdpark0723@gmail.com>
2021-03-29 16:57:48 +0900

    use time stamp

--- src/main/kotlin/io/github/siyual_park/controller/ErrorController.kt
+++ src/main/kotlin/io/github/siyual_park/controller/ErrorController.kt
@@ -10,7 +10,9 @@ import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.ModelAndView
+import java.time.Instant
 import java.util.Collections
+import java.util.Date
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -52,7 +54,8 @@ class ErrorController(
                     "Internal Server Error"
                 } else {
                     body["message"] as? String ?: ""
-                }
+                },
+                timestamp = (body["timestamp"] as? Date)?.toInstant() ?: Instant.now()
             ),
             status
         )
