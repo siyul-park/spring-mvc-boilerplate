67d13e0ca5a18fe37566be56a5df4843d2171d33
siyual-park <kdpark0723@gmail.com>
2021-03-29 09:19:46 +0900

    Refactoring

--- src/main/kotlin/io/github/siyual_park/repository/patch/JsonMergePatch.kt
+++ src/main/kotlin/io/github/siyual_park/repository/patch/JsonMergePatch.kt
@@ -1,11 +1,17 @@
 package io.github.siyual_park.repository.patch
 
+import com.fasterxml.jackson.databind.JsonMappingException
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.siyual_park.exception.BadRequestException
 
 class JsonMergePatch<T>(
     private val overrides: Any,
     private val objectMapper: ObjectMapper
 ) : Patch<T> {
 
-    override fun apply(obj: T): T = objectMapper.updateValue(obj, overrides)
+    override fun apply(obj: T): T = try {
+        objectMapper.updateValue(obj, overrides)
+    } catch (e: JsonMappingException) {
+        throw BadRequestException(e.message)
+    }
 }
