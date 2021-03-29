ccc6221ca9bfaaa711ff7354acb93c237b023401
siyual-park <kdpark0723@gmail.com>
2021-03-29 23:52:40 +0900

    Refactoring

--- src/main/kotlin/io/github/siyual_park/repository/specification/ArticleSpecification.kt
+++ src/main/kotlin/io/github/siyual_park/repository/specification/ArticleSpecification.kt
@@ -2,14 +2,11 @@ package io.github.siyual_park.repository.specification
 
 import io.github.siyual_park.model.article.Article
 import io.github.siyual_park.model.category.Category
+import io.github.siyual_park.repository.expansion.get
 import org.springframework.data.jpa.domain.Specification
-import javax.persistence.criteria.Path
 
 object ArticleSpecification {
     fun withCategory(categoryId: String) = Specification<Article> { root, _, builder ->
-        builder.equal(
-            root.get<Path<Category>>(Article::category.name).get<String>(Category::id.name),
-            categoryId
-        )
+        builder.equal(root[Article::category][Category::id], categoryId)
     }
 }
