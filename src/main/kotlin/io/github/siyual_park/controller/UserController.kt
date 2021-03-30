ec5e432a8fd7406c0d15389fa4f1027477479e99
siyual-park <kdpark0723@gmail.com>
2021-03-30 21:47:06 +0900

    Add update

--- src/main/kotlin/io/github/siyual_park/controller/UserController.kt
+++ src/main/kotlin/io/github/siyual_park/controller/UserController.kt
@@ -4,10 +4,13 @@ import io.github.siyual_park.model.user.UserCreatePayload
 import io.github.siyual_park.model.user.UserCreatePayloadMapper
 import io.github.siyual_park.model.user.UserResponsePayload
 import io.github.siyual_park.model.user.UserResponsePayloadMapper
+import io.github.siyual_park.model.user.UserUpdatePayload
 import io.github.siyual_park.repository.UserRepository
 import io.github.siyual_park.repository.patch.JsonMergePatchFactory
 import io.swagger.annotations.Api
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -28,6 +31,16 @@ class UserController(
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody payload: UserCreatePayload): UserResponsePayload {
         return userRepository.create(userCreatePayloadMapper.map(payload))
+            .let { userResponsePayloadMapper.map(it) }
+    }
+
+    @PatchMapping("/{user-id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun update(
+        @PathVariable("user-id") id: String,
+        @RequestBody payload: UserUpdatePayload
+    ): UserResponsePayload {
+        return userRepository.updateById(id, jsonMergePatchFactory.create(payload))
             .let { userResponsePayloadMapper.map(it) }
     }
 }
--- src/test/kotlin/io/github/siyual_park/controller/UserControllerTest.kt
+++ src/test/kotlin/io/github/siyual_park/controller/UserControllerTest.kt
@@ -3,16 +3,20 @@ package io.github.siyual_park.controller
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.github.siyual_park.expansion.json
 import io.github.siyual_park.expansion.readValue
+import io.github.siyual_park.factory.RandomFactory
 import io.github.siyual_park.factory.UserCreatePayloadMockFactory
 import io.github.siyual_park.model.user.UserCreatePayloadMapper
 import io.github.siyual_park.model.user.UserResponsePayload
+import io.github.siyual_park.model.user.UserUpdatePayload
 import io.github.siyual_park.repository.UserRepository
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.patch
 import org.springframework.test.web.servlet.post
+import java.util.Optional
 
 @ControllerTest
 class UserControllerTest @Autowired constructor(
@@ -37,6 +41,28 @@ class UserControllerTest @Autowired constructor(
         assertNotNull(user.id)
         assertEquals(user.name, payload.name)
         assertEquals(user.nickname, payload.nickname)
+        assertNotNull(user.createdAt)
+        assertNotNull(user.updatedAt)
+    }
+
+    @Test
+    fun testUpdate() {
+        val created = userCreatePayloadMockFactory.create()
+            .let { userCreatePayloadMapper.map(it) }
+            .let { userRepository.create(it) }
+
+        val payload = UserUpdatePayload(
+            nickname = Optional.of(RandomFactory.createString(10))
+        )
+        val result = mockMvc.patch("/users/${created.id}") { json(objectMapper.writeValueAsString(payload)) }
+            .andExpect { status { isOk() } }
+            .andReturn()
+
+        val user: UserResponsePayload = objectMapper.readValue(result.response.contentAsString)
+
+        assertNotNull(user.id)
+        assertEquals(user.name, payload.name)
+        assertEquals(user.nickname, payload.nickname?.get())
         assertNotNull(user.createdAt)
         assertNotNull(user.updatedAt)
     }
