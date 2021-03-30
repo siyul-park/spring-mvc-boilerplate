7ea7781d8132a78aed85f728df36f616faa7890b
siyual-park <kdpark0723@gmail.com>
2021-03-30 22:34:32 +0900

    fix error

--- src/main/kotlin/io/github/siyual_park/domain/security/HashEncoder.kt
+++ src/main/kotlin/io/github/siyual_park/domain/security/HashEncoder.kt
@@ -1,24 +1,26 @@
 package io.github.siyual_park.domain.security
 
 import java.security.MessageDigest
+import kotlin.experimental.and
 
-private val hexArray = "0123456789abcdef".toCharArray()
+private val digits = "0123456789ABCDEF".toCharArray()
 
 object HashEncoder {
     fun encode(value: String, algorithm: String): ByteArray {
         val messageDigest = MessageDigest.getInstance(algorithm)
-        messageDigest.update(value.toByte())
+        messageDigest.update(value.toByteArray())
 
         return messageDigest.digest()
     }
 
     fun bytesToHex(bytes: ByteArray): String {
         val hexChars = CharArray(bytes.size * 2)
-        bytes.forEachIndexed { byte, i ->
-            val v = byte.and(0xFF)
-            hexChars[i * 2] = hexArray[v.shr(4)]
-            hexChars[i * 2 + 1] = hexArray[v.and(0x0F)]
+        for (i in bytes.indices) {
+            val v = bytes[i].toInt() and 0xFF
+            hexChars[i * 2] = digits[v.ushr(4)]
+            hexChars[i * 2 + 1] = digits[v and 0x0F]
         }
+
         return String(hexChars)
     }
 }
