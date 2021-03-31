62d763ab0a7c063230953a9dad486bf73b47a5cd
siyual-park <kdpark0723@gmail.com>
2021-03-31 09:09:55 +0900

    Add UserScopeToken

--- src/main/kotlin/io/github/siyual_park/model/user/User.kt
+++ src/main/kotlin/io/github/siyual_park/model/user/User.kt
@@ -2,7 +2,6 @@ package io.github.siyual_park.model.user
 
 import io.github.siyual_park.model.BaseEntity
 import javax.persistence.Column
-import javax.persistence.ElementCollection
 import javax.persistence.Entity
 import javax.persistence.Index
 import javax.persistence.Table
@@ -16,9 +15,6 @@ data class User(
     var nickname: String,
     @Column(nullable = false)
     var password: String,
-    @ElementCollection
-    @Column(nullable = false)
-    var scope: Set<String> = setOf(),
     @Column(nullable = false)
     var hashAlgorithm: String = defaultHashAlgorithm
 ) : BaseEntity() {
