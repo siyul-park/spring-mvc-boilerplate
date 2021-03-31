0733c3803d8833b81833c64ae1fe1eadbe0e22fd
siyual-park <kdpark0723@gmail.com>
2021-04-01 00:18:25 +0900

    Cache

--- src/main/kotlin/io/github/siyual_park/domain/security/BearerAuthenticationFactory.kt
+++ src/main/kotlin/io/github/siyual_park/domain/security/BearerAuthenticationFactory.kt
@@ -2,14 +2,14 @@ package io.github.siyual_park.domain.security
 
 import io.github.siyual_park.exception.ExpiredTokenException
 import io.github.siyual_park.model.token.TokenAuthentication
-import io.github.siyual_park.repository.UserRepository
+import io.github.siyual_park.repository.CachedUserRepository
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
 
 @Component
 class BearerAuthenticationFactory(
     private val tokenExchanger: TokenExchanger,
-    private val userRepository: UserRepository
+    private val userRepository: CachedUserRepository
 ) : AuthenticationFactory {
     override fun create(credentials: String): Authentication {
         val token = tokenExchanger.decode(credentials)
