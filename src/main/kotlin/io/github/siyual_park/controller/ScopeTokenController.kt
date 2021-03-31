aad1738621c3f00f98d2dff560e46cd6f646e098
siyual-park <kdpark0723@gmail.com>
2021-03-31 10:29:47 +0900

    Add find method

--- src/main/kotlin/io/github/siyual_park/controller/ScopeTokenController.kt
+++ src/main/kotlin/io/github/siyual_park/controller/ScopeTokenController.kt
@@ -1,5 +1,6 @@
 package io.github.siyual_park.controller
 
+import io.github.siyual_park.domain.Paginator
 import io.github.siyual_park.domain.scope.ScopeFetchExecutor
 import io.github.siyual_park.domain.scope.ScopeResponsePayloadMapper
 import io.github.siyual_park.domain.scope.ScopeTokenCreatePayloadMapper
@@ -10,12 +11,18 @@ import io.github.siyual_park.repository.ScopeTokenRelationRepository
 import io.github.siyual_park.repository.ScopeTokenRepository
 import io.github.siyual_park.repository.UserScopeTokenRepository
 import io.swagger.annotations.Api
+import org.springframework.data.domain.Sort
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import java.util.stream.Stream
 
 @Api
 @RestController
@@ -29,6 +36,8 @@ class ScopeTokenController(
     private val scopeTokenCreatePayloadMapper: ScopeTokenCreatePayloadMapper,
 
 ) {
+
+    private val paginator = Paginator.of(scopeTokenRepository, scopeResponsePayloadMapper)
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
@@ -44,5 +53,36 @@ class ScopeTokenController(
         }
 
         return scopeResponsePayloadMapper.map(scopeToken)
+    }
+
+    @GetMapping("")
+    fun findAll(
+        @RequestParam("page", required = false) offset: Int?,
+        @RequestParam("per_page", required = false) limit: Int?,
+        @RequestParam("sort", required = false) property: String?,
+        @RequestParam("order", required = false) direction: Sort.Direction?
+    ): ResponseEntity<Stream<ScopeTokenResponsePayload>> {
+        return paginator.query(
+            offset = offset,
+            limit = limit,
+            sort = Sort.by(
+                direction ?: Sort.Direction.ASC,
+                property ?: "id"
+            )
+        )
+    }
+
+    @GetMapping("/{scope-token-id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun findById(@PathVariable("scope-token-id") id: String): ScopeTokenResponsePayload {
+        return scopeTokenRepository.findByIdOrFail(id)
+            .let { scopeResponsePayloadMapper.map(it) }
+    }
+
+    @GetMapping("/@{category-name}")
+    @ResponseStatus(HttpStatus.OK)
+    fun findByName(@PathVariable("scope-token-name") name: String): ScopeTokenResponsePayload {
+        return scopeTokenRepository.findByNameOrFail(name)
+            .let { scopeResponsePayloadMapper.map(it) }
     }
 }
